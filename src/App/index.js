14303a0cded13fb476d2fac3efddc3c2ab6e3a06
bhayek12@gmail.com <bhayek12@gmail.com>
2019-02-06 11:13:55 -0700

    Add todo function

--- src/App/index.js
+++ src/App/index.js
@@ -4,6 +4,11 @@ import './App.css';
 const App = () => {
   const [todos, setTodos] = useState([])
 
+  const addTodo = text => { 
+    const newTodos = [...todos, { text }]; 
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <h1>Hooks</h1>
